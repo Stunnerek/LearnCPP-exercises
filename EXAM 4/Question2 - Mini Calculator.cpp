d45adc2c1cbceaa74cc764608a736b3bd29a3a8c
Kamil <78850014+Stunnerek@users.noreply.github.com>
2023-12-05 22:20:57 +0100

    Exam 4 quizzes
    Very fun tasks, i enjoyed falling ball one

---
+++ EXAM 4/Question2 - Mini Calculator.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+int toint(int a) {
+    return a;
+}
+
+int main()
+{
+    double a{};
+    double b{};
+    char equation{};
+    
+    std::cout << "Enter a double value";
+    std::cin >> a;
+    std::cout << "Enter a double value";
+    std::cin >> b;
+    std::cout << "Enter + , -, *, or /:*";
+    std::cin >> equation;
+    if (equation == '+') {
+        std::cout << a << equation << b << "=" << a + b;
+    }
+    else if (equation == '-') {
+        std::cout << a << equation << b << "=" << a - b;
+    }
+    else if (equation == '*') {
+        std::cout << a << equation << b << "=" << a * b;
+    }
+    else if (equation == '/') {
+        std::cout << a << equation << b << "=" << a / b;
+    }
+    else {
+        std::cout << "bruh";
+    }
+
+}
---
+++ EXAM 4/Question3 - Falling Ball.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+int getHeight() {
+	int a;
+	std::cout << "Enter the height of the tower in meters:";
+	std::cin >> a;
+	return a;
+}
+int Fall(int a) {
+
+	int b{ a };
+	int seconds{ 0 };
+	
+	double gravity{ 9.8 };
+	while (b > 0) {
+		
+		
+		double height{ b - (gravity*(seconds*seconds)) /2  };
+		if (height <= 0) {
+			std::cout << "At " << seconds << " seconds, the ball is at the ground"  "\n";
+			b = 0;
+			return NULL;
+			 
+
+		}
+		std::cout << "At" << seconds <<"seconds, the ball is at height" << height << "\n";
+		
+		seconds++;
+		b--;
+		
+
+	}
+	return NULL;
+}
+
+int main()
+{
+	int Height{ getHeight() };
+	Fall(Height);
+   
+}
+
